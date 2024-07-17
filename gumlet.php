af6a81351f429a60f6e5e9608fb368902a8d2bd3
akbansa <bansalanshul11@yahoo.com>
2024-07-17 15:03:32 +0530

    added support for custom parameters from the logged in user

--- gumlet.php
+++ gumlet.php
@@ -31,12 +31,99 @@ if (!defined('GUMLET_PLUGIN_VERSION')) {
     define('GUMLET_PLUGIN_VERSION', '1.0');
 }
 
+function gumlet_video_shortcode($atts)
+{
+    $watermark = get_option('gumlet_video_settings');
+    $sc_args = shortcode_atts(
+        array(
+            'width' => '100%',
+            'height' => '100%',
+            'cc_enabled' => get_option('gumlet_default_cc_enabled'),
+            'id'    => 'id',
+            'annotate'=> true,
+            'autoplay'=> false,
+            'loop'=> false,
+            'controls'=> 'on',
+        ),
+        $atts
+    );
+    $width = $sc_args['width'];
+    $height = $sc_args['height'];
+    $id = $sc_args['id'];
+    $annotate = $sc_args['annotate'];
+    $cc_enabled = $sc_args['cc_enabled'];
+
+    if (!$atts['id']) {
+        return "Required argument id for embedded video not found.";
+    } else {
+        $video = $id;
+    }
+    
+    $watermark_text = '';
+    if($annotate) {
+        $current_user = wp_get_current_user();
+        if(isset($watermark['dynamic_watermark_name']) && $watermark['dynamic_watermark_name']) {
+            $watermark_text .= $current_user->display_name . " ";
+        }
+        if(isset($watermark['dynamic_watermark_email']) && $watermark['dynamic_watermark_email']) {
+            $watermark_text .= $current_user->user_email. " ";
+        }
+        if(isset($watermark['dynamic_watermark_user_id']) && $watermark['dynamic_watermark_user_id']) {
+            $watermark_text .= $current_user->ID;
+        }
+    }
+   
+    $uniq = 'u' . rand();
+    // $url = "https://play.gumlet.io/embed/$video?otp=$OTP&playbackInfo=$playbackInfo";
+    $url = "https://play.gumlet.io/embed/$video?";
+    if ($sc_args['autoplay']) {
+        $url .= "autoplay=true&";
+    }
+    if ($sc_args['loop']) {
+        $url .= "loop=true&";
+    }
+    if ($sc_args['controls'] == 'off') {
+        $url .= "disable_player_controls=false&";
+    }
+    if(!!$cc_enabled) {
+        $url .= "caption=true&";
+    }
+    if($watermark_text != '') {
+        $url .= "watermark_text=".$watermark_text;
+    }
+    if($width != "100%") {
+        $opening_div = '<div>';
+        $style = 'width="'.$width.'" height="'.$height.'" style="border:none;"';
+        $closing_div = '</div>';
+    } else {
+        $opening_div = '<div style="padding:56.25% 0 0 0;position:relative;">';
+        $style = 'style="border:none; position: absolute; top:0; left:0; height: 100%; width: 100%;"';
+        $closing_div = '</div>';
+    }
+    
+    $output = <<<END
+    
+        $opening_div
+        <iframe
+        src="$url" id="$uniq" loading="lazy" title="Gumlet video player"
+        $style
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;" frameborder="0"></iframe>
+        $closing_div
+        <script>
+        </script>
+    END;
+        
+    return $output;
+}
+
+add_shortcode('gumlet', 'gumlet_video_shortcode');
+
 
 
 function gumlet_video_plugin_admin_action_links($links, $file)
 {
     if ($file === plugin_basename(__FILE__)) {
-        $settings_link = '<a href="options-general.php?page=gumlet-options">Settings</a>';
+        $settings_link = '<a href="options-general.php?page=gumlet-video-options">Settings</a>';
         array_unshift($links, $settings_link);
     }
     return $links;
@@ -51,6 +138,6 @@ function gumlet_video_plugin_activate()
 {
     // plugin activation code here...
     if (!get_option('gumlet_video_settings')) {
-        update_option('gumlet_video_settings', ["lazy_load" => 1, "original_images" => 1, "auto_compress"=> 1, "server_webp"=> 0]);
+        update_option('gumlet_video_settings', ["dynamic_watermark" => 0, "dynamic_watermark_email" => 0, "dynamic_watermark_name"=> 0, "dynamic_watermark_user_id"=> 0]);
     }
 }
--- includes/video-options.php
+++ includes/video-options.php
@@ -46,47 +46,21 @@ class Gumlet_Video_Settings
     public function gumlet_options_page()
     {
         ?>
-        <!-- <head>
-          <link rel="stylesheet" type="text/css" href="gumlet.css">
-        </head> -->
         <div class="wrap">
     <h1>
         <img src="<?php echo plugins_url('includes/assets/images/gumlet-logo.png', __DIR__); ?>" alt="gumlet Logo"
             style="width:200px; margin-left: -12px;">
     </h1>
-    <!-- <?php if( isset($_GET['settings-updated']) ) { ?>
-    <div class="notice notice-warning">
-        <p><strong>Heads up! Clear cache:</strong> We recommend you clear cache after enabling Gumlet.</p>
-    </div>
-    <?php } ?> -->
-    <!-- <div class="notice notice-info">
-        <p><strong>Important!</strong> Gumlet <strong>does not</strong> work well with other lazy-load plugins. We
-            recommend you <strong>disable</strong> all other lazy-load plugins and lazy-load settings in themes.</p>
-        <p><strong>Need help getting started?</strong> It's easy! Check out our
-            <a href="https://docs.gumlet.com/docs/image-integration-wordpress" target="_blank">instructions.</a>
-        </p>
-    </div> -->
-
+    
     <form method="post" action="<?php echo admin_url('options.php'); ?>">
 
         <?php settings_fields('gumlet_video_settings_group'); ?>
         <div class="mytabs">
-            <input type="radio" id="tabBasic" name="mytabs" checked="checked">
-            <label for="tabBasic" class="mytablabel">Basic</label>
+            <input type="radio" id="tabAdvanced" name="mytabs" checked>
+            <label for="tabAdvanced" class="mytablabel">Settings</label>
             <div class="tab">
                 <table class="form-table">
                     <tbody>
-                        <tr>
-                            <th>
-                                <label class="description" for="gumlet_min_width">
-                                    <?php esc_html_e('Width', 'gumlet'); ?>
-                                </label>
-                            </th>
-                            <td>
-                                <input id="gumlet_min_width" type="number" name="gumlet_min_width" min="0" max="1920" value="<?php echo get_option('gumlet_min_width'); ?>" />
-                                <p style="color: #666">If set, this will be minimum width for video player.</p>
-                            </td>
-                        </tr>
                         <tr>
                             <th>
                                 <label class="description" for="gumlet_default_cc_enabled">
@@ -99,46 +73,13 @@ class Gumlet_Video_Settings
                                 <p style="color: #666">If this is enabled, the videos having subtitles will show it by default.</p>
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-            </div>
-            <input type="radio" id="tabAdvanced" name="mytabs">
-            <label for="tabAdvanced" class="mytablabel">Settings</label>
-            <div class="tab">
-                <table class="form-table">
-                    <tbody>
-                        <tr>
-                            <th>
-                                <label class="description" for="gumlet_video_settings[api_key]">
-                                    <?php esc_html_e('Gumlet API Key', 'gumlet'); ?> *
-                                </label>
-                            </th>
-                            <td>
-                                <input id="gumlet_video_settings[api_key]" type="password" name="gumlet_video_settings[api_key]"
-                                    placeholder="******************"
-                                    value="<?php echo $this->get_option('api_key'); ?>" required="required"
-                                    class="regular-text code" />
-                                <p style="color: #666">&nbsp;Get the API key from <a href="https://dashboard.gumlet.com/user/apikey" target="_blank">https://dashboard.gumlet.com/user/apikey</a></p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                <label class="description" for="gumlet_video_settings[dynamic_watermark]">
-                                    <?php esc_html_e('Dynamic Watermark', 'gumlet'); ?>
-                                </label>
-                            </th>
-                            <td>
-                                <input id="gumlet_video_settings[dynamic_watermark]" type="checkbox" name="gumlet_video_settings[dynamic_watermark]"
-                                    value="1" <?php checked(1, $this->get_option('dynamic_watermark')) ?> />
-                            </td>
-                        </tr>
                         <script>
                             let options = <?php echo json_encode($this->options); ?>
                         </script>
                         <tr id="dynamic_watermark_options">
                             <th>
                                 <label class="description" for="">
-                                    <?php esc_html_e('Watermark Options', 'gumlet'); ?>
+                                    <?php esc_html_e('Dynamic Watermark Options', 'gumlet'); ?>
                                 </label>
                             </th>
                             <td>
@@ -160,18 +101,20 @@ class Gumlet_Video_Settings
                                 </label>
                                 <input id="gumlet_video_settings[dynamic_watermark_user_id]" type="checkbox" name="gumlet_video_settings[dynamic_watermark_user_id]"
                                     value=1 <?php checked(1, $this->get_option('dynamic_watermark_user_id')) ?> />
+
+                                <p style="color: #666">For collections with dynamic watermarking enabled, the above selected options will show over the video.</p>
                             </td>
                         </tr>
                     </tbody>
                 </table>
                 <script>
-                const dynamicElem = document.getElementById("gumlet_video_settings[dynamic_watermark]");
-                dynamicElem.addEventListener("change", hideShowOptions);
-                function hideShowOptions() {
-                    let hideShowStyle = dynamicElem.checked ? "table-row": "none"
-                    document.getElementById("dynamic_watermark_options").style.display = hideShowStyle;
-                }
-                hideShowOptions(dynamicElem)
+                // const dynamicElem = document.getElementById("gumlet_video_settings[dynamic_watermark]");
+                // dynamicElem.addEventListener("change", hideShowOptions);
+                // function hideShowOptions() {
+                //     let hideShowStyle = dynamicElem.checked ? "table-row": "none"
+                //     document.getElementById("dynamic_watermark_options").style.display = hideShowStyle;
+                // }
+                // hideShowOptions(dynamicElem)
             </script>
             </div>
             
@@ -226,7 +169,7 @@ class Gumlet_Video_Settings
      */
     public function gumlet_add_options_link()
     {
-        add_options_page('Gumlet', 'Gumlet', 'manage_options', 'gumlet-options', [ $this, 'gumlet_options_page' ]);
+        add_options_page('Gumlet', 'Gumlet', 'manage_options', 'gumlet-video-options', [ $this, 'gumlet_options_page' ]);
     }
 
     /**
@@ -236,7 +179,7 @@ class Gumlet_Video_Settings
     {
         register_setting('gumlet_video_settings_group', 'gumlet_video_settings');
         register_setting('gumlet_video_settings_group', 'gumlet_default_cc_enabled', ["type"=> 'boolean', "default"=>true]);
-        register_setting('gumlet_video_settings_group', 'gumlet_min_width', ["type"=> 'integer']);
+        // register_setting('gumlet_video_settings_group', 'gumlet_responsive', ["type"=> 'boolean', "default"=>true]);
     }
 
     /**
