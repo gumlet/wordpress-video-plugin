db3f2c1c1b70f6810eaab6f216f48f7046192c2a
akbansa <bansalanshul11@yahoo.com>
2024-09-27 17:03:00 +0530

    fix error

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-
- /*
- * Plugin Name:       Gumlet Video
- * Description:       Video Hosting Plugin for WordPress
- * Plugin URI:        https://github.com/gumlet/wordpress-video-plugin
- * Version:           1.0
- * Author:            Gumlet
- * Author URI:        https://www.gumlet.com
- * License:           GPLv2
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- */
-
 function gumlet_oembed_provider() {
     if ( ! function_exists( 'wp_oembed_add_provider' ) ) {
 
