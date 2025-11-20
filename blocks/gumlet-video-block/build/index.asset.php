7d64cd94dad4859420a9988a1ff1ab980f6218b8
akbansa <bansalanshul11@yahoo.com>
2025-11-20 21:12:36 +0530

    updated build for block

--- blocks/gumlet-video-block/build/index.asset.php
+++ blocks/gumlet-video-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '409f288cf6f237cde4ae');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a78e64f2420226ede821');
